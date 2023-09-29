558d6cf3659eb75c5457b79fb813af5cf7286019
N8hax <karimarbad@gmail.com>
2023-09-29 14:51:15 +0300

    Initial Setup

---
+++ SBE/SBE.Logic/Abstraction/IMessageCodec.cs
@@ -0,0 +1,10 @@
+﻿using SBE.Logic.Models;
+
+namespace SBE.Logic.Abstraction
+{
+    public interface IMessageCodec
+    {
+        byte[] Encode(Message message);
+        Message Decode(byte[] data);
+    }
+}
---
+++ SBE/SBE.Logic/Models/Message.cs
@@ -0,0 +1,8 @@
+﻿namespace SBE.Logic.Models
+{
+    public class Message
+    {
+        public Dictionary<string, string> Headers { get; set; } = new Dictionary<string, string>();
+        public byte[] Payload { get; set; } = new byte[0];
+    }
+}
